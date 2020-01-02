4430db503b008822e9f8ad537b477f59c2b5ead7
soojung <blessme333@gmail.com>
2020-01-02 18:26:36 +0900

    피자가가게 근거 데이터 클래스 설정

---
+++ app/src/main/java/com/soo/a20200102_practice_pizzaoderapp/datas/PizzaStore.kt
@@ -0,0 +1,9 @@
+package com.soo.a20200102_practice_pizzaoderapp.datas
+
+class PizzaStore(storeName:String, phoneNum:String, logoUrl:String) {
+
+    var storeName = storeName
+    var phoneNum = phoneNum
+    var logoUrl = logoUrl
+
+}
