faacd22192ab33aa565b11a9d5021685f071c110
soojung <blessme333@gmail.com>
2020-01-02 18:33:39 +0900

    메인 액티비티에 피자헛 목록으로 추가

--- app/src/main/java/com/soo/a20200102_practice_pizzaoderapp/MainActivity.kt
+++ app/src/main/java/com/soo/a20200102_practice_pizzaoderapp/MainActivity.kt
@@ -2,18 +2,31 @@ package com.soo.a20200102_practice_pizzaoderapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.soo.a20200102_practice_pizzaoderapp.datas.PizzaStore
 
 class MainActivity : BaseActivity() {
-    override fun setupEvents() {
 
-    }
+    var pizzaStoreList = ArrayList<PizzaStore>()
 
-    override fun setValues() {
-
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+
     }
+
+    override fun setupEvents() {
+
+
+    }
+
+    override fun setValues() {
+        addPizzaStores()
+    }
+
+    fun addPizzaStores() {
+        pizzaStoreList.add(PizzaStore("피자헛", "010-123-4567", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg"))    }
+
 }
