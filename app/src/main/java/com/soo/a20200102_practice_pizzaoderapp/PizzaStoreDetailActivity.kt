dd6b475ad8c01ed6042e1068418f31e8cb9063d5
soojung <blessme333@gmail.com>
2020-01-02 20:46:14 +0900

    가게 상세 화면 초안 작성

---
+++ app/src/main/java/com/soo/a20200102_practice_pizzaoderapp/PizzaStoreDetailActivity.kt
@@ -0,0 +1,25 @@
+package com.soo.a20200102_practice_pizzaoderapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PizzaStoreDetailActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pizza_store_detail)
+        setupEvents()
+        setValues()
+    }
+
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
